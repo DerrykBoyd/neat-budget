fd783cc7f18d6b0cd1cc8540b00db8b7d899cb6e
Derryk Boyd <derryk@redmantech.com>
2020-12-12 20:50:26 -0700

    Fix routing reload

--- src/Routes.svelte
+++ src/Routes.svelte
@@ -1,20 +1,17 @@
 <script>
   import { Router, Link, Route } from "svelte-routing";
   import Home from "./routes/Home.svelte";
- 
-  export let url = "";
+  import About from "./routes/About.svelte"
+  export let url = ""
 </script> 
  
 <Router url="{url}">
   <nav>
     <Link to="/">Home</Link>
     <Link to="about">About</Link>
-    <Link to="blog">Blog</Link>
   </nav>
   <div>
-    <!--<Route path="blog/:id" component="{BlogPost}" />
-    <Route path="blog" component="{Blog}" />
-    <Route path="about" component="{About}" /> -->
     <Route path="/"><Home /></Route>
+    <Route path="about" component="{About}" />
   </div>
 </Router>
---
+++ src/routes/About.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let location
+  console.log(location)
+</script>
+
+<style>
+  /* your styles go here */
+</style>
+
+<h1 class="text-lg">About Page {location.href}</h1>
