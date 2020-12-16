2b3937b5003b11419793f07f4941a86920e24f7f
Derryk Boyd <derryk@redmantech.com>
2020-12-16 01:07:48 -0700

    Add rollup replace plugin

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import includePaths from "rollup-plugin-includepaths";
 import sveltePreprocess from "svelte-preprocess";
+import replace from "rollup-plugin-replace";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -46,6 +47,9 @@ export default {
     file: "public/build/bundle.js",
   },
   plugins: [
+    replace({
+      _ENVIRONMENT: production ? "prod" : "dev",
+    }),
     includePaths(includePathOptions),
     svelte({
       compilerOptions: {
