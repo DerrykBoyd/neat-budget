ed51177eb44f3eae2bd46348d563041a6cb7a325
Derryk Boyd <derrykboyd@gmail.com>
2021-01-02 22:27:31 -0700

    Test alternate netlify env setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,35 +10,36 @@ const netlify = process.env.NETLIFY === "true";
 
 console.log(process.env.FIREBASE_API_KEY_DEV);
 
-const config = netlify
-  ? {
-      apiKey: JSON.stringify(process.env.FIREBASE_API_KEY_DEV),
-      authDomain: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN_DEV),
-      projectId: JSON.stringify(process.env.FIREBASE_PROJECT_ID_DEV),
-      storageBucket: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET_DEV),
-      messagingSenderId: JSON.stringify(process.env.FIREBASE_MESSAGING_ID_DEV),
-      appId: JSON.stringify(process.env.FIREBASE_APP_ID_DEV),
-      measurementId: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID_DEV),
-    }
-  : {
-      apiKey: production ? _process.env.FIREBASE_API_KEY : _process.env.FIREBASE_API_KEY_DEV,
-      authDomain: production
-        ? _process.env.FIREBASE_AUTH_DOMAIN
-        : _process.env.FIREBASE_AUTH_DOMAIN_DEV,
-      projectId: production
-        ? _process.env.FIREBASE_PROJECT_ID
-        : _process.env.FIREBASE_PROJECT_ID_DEV,
-      storageBucket: production
-        ? _process.env.FIREBASE_STORAGE_BUCKET
-        : _process.env.FIREBASE_STORAGE_BUCKET_DEV,
-      messagingSenderId: production
-        ? _process.env.FIREBASE_MESSAGING_ID
-        : _process.env.FIREBASE_MESSAGING_ID_DEV,
-      appId: production ? _process.env.FIREBASE_APP_ID : _process.env.FIREBASE_APP_ID_DEV,
-      measurementId: production
-        ? _process.env.FIREBASE_MEASUREMENT_ID
-        : _process.env.FIREBASE_MEASUREMENT_ID_DEV,
-    };
+let config = {
+  apiKey: production ? _process.env.FIREBASE_API_KEY : _process.env.FIREBASE_API_KEY_DEV,
+  authDomain: production
+    ? _process.env.FIREBASE_AUTH_DOMAIN
+    : _process.env.FIREBASE_AUTH_DOMAIN_DEV,
+  projectId: production ? _process.env.FIREBASE_PROJECT_ID : _process.env.FIREBASE_PROJECT_ID_DEV,
+  storageBucket: production
+    ? _process.env.FIREBASE_STORAGE_BUCKET
+    : _process.env.FIREBASE_STORAGE_BUCKET_DEV,
+  messagingSenderId: production
+    ? _process.env.FIREBASE_MESSAGING_ID
+    : _process.env.FIREBASE_MESSAGING_ID_DEV,
+  appId: production ? _process.env.FIREBASE_APP_ID : _process.env.FIREBASE_APP_ID_DEV,
+  measurementId: production
+    ? _process.env.FIREBASE_MEASUREMENT_ID
+    : _process.env.FIREBASE_MEASUREMENT_ID_DEV,
+};
+
+console.log(config);
+
+if (netlify)
+  config = {
+    apiKey: JSON.stringify(process.env.FIREBASE_API_KEY_DEV),
+    authDomain: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN_DEV),
+    projectId: JSON.stringify(process.env.FIREBASE_PROJECT_ID_DEV),
+    storageBucket: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET_DEV),
+    messagingSenderId: JSON.stringify(process.env.FIREBASE_MESSAGING_ID_DEV),
+    appId: JSON.stringify(process.env.FIREBASE_APP_ID_DEV),
+    measurementId: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID_DEV),
+  };
 
 console.log(config);
 
