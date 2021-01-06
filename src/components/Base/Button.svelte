8d5429c060c4b2a70eaf034fda0479ed28c31153
Derryk Boyd <derrykboyd@gmail.com>
2021-01-05 23:42:13 -0700

    Add accounts to sidebar list

--- src/components/Base/Button.svelte
+++ src/components/Base/Button.svelte
@@ -4,26 +4,32 @@
   export let disabled = false;
   export let color = "green";
   export let noMargin = false;
+  export let inverted = false;
 
   let bgColor = "bg-green-600";
-  let hoverColor = "hover:bg-green-700";
-  let textColor = "text-white";
+  let hoverColor = inverted ? "hover:bg-green-200" : "hover:bg-green-700";
+  let textColor = inverted ? "text-green-700" : "text-white";
+  let border = "";
 
   $: switch (color) {
     case "red":
-      bgColor = "bg-red-600";
-      hoverColor = "hover:bg-red-700";
+      bgColor = inverted ? "bg-transparent" : "bg-red-600";
+      border = "border border-red-600 rounded";
+      hoverColor = inverted ? "hover:bg-red-200" : "hover:bg-red-700";
+      textColor = inverted ? "text-red" : "text-white";
       break;
     case "grey":
     case "gray":
-      bgColor = "bg-gray-200";
-      hoverColor = "hover:bg-gray-300";
-      textColor = "text-gray-800";
+      bgColor = inverted ? "bg-transparent" : "bg-gray-200";
+      border = "border border-gray-200 rounded";
+      hoverColor = inverted ? "hover:bg-gray-200" : "hover:bg-gray-300";
+      textColor = inverted ? "text-white" : "text-gray-800";
       break;
     case "white":
-      bgColor = "white";
-      hoverColor = "hover:bg-gray-50";
-      textColor = "text-gray-800";
+      bgColor = inverted ? "bg-transparent" : "white";
+      border = "border border-gray-200 rounded";
+      hoverColor = inverted ? "hover:border-white hover:text-white" : "hover:bg-gray-50";
+      textColor = inverted ? "text-gray-200" : "text-gray-800";
       break;
   }
 </script>
@@ -37,7 +43,7 @@
   {id}
   {disabled}
   on:click={handleClick}
-  class={`${disabled ? 'opacity-30 pointer-events-none' : 'opacity-100'} ${hoverColor} ${textColor} ${noMargin ? '' : 'my-2 mr-2'} inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium ${bgColor}`}>
+  class={`${disabled ? 'opacity-30 pointer-events-none' : 'opacity-100'} ${border} ${hoverColor} ${textColor} ${noMargin ? '' : 'my-2 mr-2'} inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium ${bgColor}`}>
   <slot name="icon" />
   <slot />
 </button>
--- src/components/Budget/Sidebar.svelte
+++ src/components/Budget/Sidebar.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Button from "../Base/Button.svelte";
+
   export let currentBudget;
 </script>
 
@@ -10,6 +12,13 @@
     min-height: 56px;
     max-height: 56px;
     overflow: hidden;
+  }
+  .sidebar-link {
+    @apply px-4 py-2;
+    cursor: pointer;
+  }
+  .sidebar-link:hover {
+    @apply bg-green-700;
   }
   @media (min-width: 1024px) {
     .sidebar {
@@ -24,13 +33,22 @@
     {currentBudget.name}
   </div>
   <div class="sidebar-links">
-    <div class="hover:bg-green-700 p-4 h-14">Budget Link</div>
-    <div class="p-4 h-14">All Accounts Link</div>
+    <div class="sidebar-link">Budget Link</div>
+    <div class="sidebar-link">All Accounts Link</div>
   </div>
   <div class="sidebar-accounts grid grid-cols-1 overflow-y-auto">
-    <div class="p-4">On Budget Accounts</div>
-    <button class="ml-4">Add Account</button>
-    <div class="p-4">Off Budget Accounts</div>
-    <button class="ml-4">Add Account</button>
+    <div class="px-4 pt-2 pb-1 underline font-medium">On Budget Accounts</div>
+    {#each currentBudget.accounts as account}
+      {#if account.onBudget}
+        <div class="sidebar-link">{account.name}</div>
+      {/if}
+    {/each}
+    <div class="px-4">
+      <Button inverted color="white">+ Add Account</Button>
+    </div>
+    <div class="px-4 pt-2 pb-1 underline font-medium">Off Budget Accounts</div>
+    <div class="px-4">
+      <Button inverted color="white">+ Add Account</Button>
+    </div>
   </div>
 </div>
